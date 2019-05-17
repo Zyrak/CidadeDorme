99c71cddd4c4186f9d0b07c6f5fdc2a414c2c69f
Zyrak <zyrak.br@gmail.com>
2019-05-16 21:20:46 -0300

    CidadeDorme

---
+++ app/src/main/java/einstein/com/br/cidadedorme/TelaCidadeDorme.java
@@ -0,0 +1,13 @@
+package einstein.com.br.cidadedorme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaCidadeDorme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_cidade_dorme);
+    }
+}
---
+++ app/src/main/java/einstein/com/br/cidadedorme/TelaResultado.java
@@ -0,0 +1,13 @@
+package einstein.com.br.cidadedorme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_resultado);
+    }
+}
